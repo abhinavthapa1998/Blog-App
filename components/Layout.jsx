c035697b209c9bd5f628ad53ef308e4e26744326
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-04-19 14:06:51 +0530

    removed image

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,11 @@
-import Link from "next/link";
+import Logo from "../src/images/journal.png";
 import { useRouter } from "next/router";
 const Layout = () => {
   const router = useRouter();
   return (
     <>
       <div className="navHeader" onClick={() => router.push("/")}>
-        <img
-          className="navImage"
-          src="https://cdn-icons.flaticon.com/png/512/2708/premium/2708780.png?token=exp=1650354471~hmac=7d6a3f7f8a1e9ff7c5e6dabc4684012e"
-          alt="logo"
-        />
+        Your Journal
       </div>
       <div className="navLink">
         <a className="hover-underline-animation" href="/">
